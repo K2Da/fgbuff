b159a495572b01503036275047faf6a7a59e4c26
K2Da <id.k2da@gmail.com>
2016-10-18 21:53:51 +0900

    create html file cache

--- server.py
+++ server.py
@@ -1,39 +1,41 @@
-from bottle import Bottle, run, template
+from bottle import Bottle, run, template, request
+from util.cache import cache
 from model.Pool import Pool
+import config
 
 app = Bottle()
 
 
 @app.route('/players')
 def players():
-    return template('players', pool=Pool.init_for_players())
+    return cache(request.path, template('players', pool=Pool.init_for_players()))
 
 
 @app.route('/tournaments')
 def tournaments():
-    return template('tournaments', pool=Pool.init_for_tournaments())
+    return cache(request.path, template('tournaments', pool=Pool.init_for_tournaments()))
 
 
 @app.route('/tournament/<challo_url>')
 def tournament(challo_url):
     fg_tournament, pool = Pool.init_for_tournament(challo_url)
-    return template('tournament', pool=pool, fg_tournament=fg_tournament)
+    return cache(request.path, template('tournament', pool=pool, fg_tournament=fg_tournament))
 
 
 @app.route('/vs/<p1>/<p2>')
 def vs(p1, p2):
     p1_id, p2_id, pool = Pool.init_for_vs(p1, p2)
-    return template('vs', pool=pool, p1=p1_id, p2=p2_id)
+    return cache(request.path, template('vs', pool=pool, p1=p1_id, p2=p2_id))
 
 
 @app.route('/player/<player_url>')
 def player(player_url):
     player_id, pool = Pool.init_for_player(player_url)
-    return template('player', pool=pool, player_id=player_id)
+    return cache(request.path, template('player', pool=pool, player_id=player_id))
 
 
 @app.route('/')
 def index():
-    return template('index')
+    return cache(request.path, template('index'))
 
-run(app, host='localhost', port=8080)
+run(app, host=config.host, port=config.port)
